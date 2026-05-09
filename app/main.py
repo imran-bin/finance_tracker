4b3add6fa75c1b3c3be72756e394461aaf983bcf
imran-bin <engg.imran22@gmail.com>
2026-05-10 00:40:16 +0600

    cors issue fixed

--- app/main.py
+++ app/main.py
@@ -36,7 +36,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "https://finance-tracker-frontend-tfbl.onrender.com" # Adding potential production origin if exists
+        "https://finance-tracker-frontend-0y2c.onrender.com",
+        "https://finance-tracker-frontend-tfbl.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
